b210ce478b58fc750d9e9f3d69dcf03b276a5f00
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-07-15 21:20:33 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,9 +12,7 @@ function frameIt() {
 }
 function submitIt() {
  $('form').on("submit", function() {
-   if ($('input:first').val()==="correct") { 
-     alert('Your form is going to be submitted now.')
-   }
+   alert('Your form is going to be submitted now.')
  })
 }
 $(document).ready(function(){
